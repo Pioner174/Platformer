b15b6aed2332c71e5515596a6255a0b9e3be05b7
Pioner174 <Romashka951@gmail.com>
2021-02-17 23:44:01 +0500

    Works with player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
+    [SerializeField]private float speedX = -1f;
+    [SerializeField]private float speedY =  0f;
+    private Rigidbody2D rb;
 
+    const float SpeedMultyplier = 50f;
+    
     void Start()
     {
      rb =GetComponent<Rigidbody2D>();
      rb.velocity = new Vector2(1f, 2f);    
     }
 
+    void Update()
+    {
+        
+    }
    
+   void FixedUpdate()
+   {
+       
+       rb.velocity = new Vector2(speedX * SpeedMultyplier * Time.fixedDeltaTime, rb.velocity.y);
+   }
 }
