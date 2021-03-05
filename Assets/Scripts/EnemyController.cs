a45e5e2500dc9a72f4b390ec6d27fb2bc27d030a
Roman <r.matanin@rfsrf.ru>
2021-03-05 16:18:18 +0500

    42 work with enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float walkDistance = 6f;
+    [SerializeField] private float walkSpeed = 1f;
+    [SerializeField] private float timeToWait = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 LeftBoundaryPosition;
+    private Vector2 RightBoundaryPosition;
+
+    private void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        leftBoundaryPosition = transform.position;
+        rightBoundaryPosition = leftBoundaryPosition + Vector2.right
+    }
+}
