df28f75ca7d404e985fcbf2291f18bc0b38cd0ec
Pioner174 <Romashka951@gmail.com>
2021-03-08 14:22:10 +0500

    47 enemy patrolling terrain, optimized

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,28 +12,56 @@ public class EnemyController : MonoBehaviour
     private Vector2 _LeftBoundaryPosition;
     private Vector2 _RightBoundaryPosition;
     private bool _isFacingRight = true;
-
     private bool _isWait = false;
+    private float _waitTime;
     private void Start(){
         _rb = GetComponent<Rigidbody2D>();
         _LeftBoundaryPosition = transform.position;
         _RightBoundaryPosition = _LeftBoundaryPosition + Vector2.right * walkDistance;
+        _waitTime = timeToWait;
     }
     private void Update() {
-        bool isOutOfRightBoundary = _isFacingRight && transform.position.x >= _RightBoundaryPosition.x;
-        bool isOutOfLeftBoundary = !_isFacingRight && transform.position.x <= _LeftBoundaryPosition.x;
-        if(isOutOfRightBoundary || isOutOfLeftBoundary){
+        if(_isWait){
+            Wait();
+            }
+        
+
+        
+        if(ShouldWait()){
             _isWait = true;
         }
     }
     private void FixedUpdate() {
+        Vector2 nextPoint = Vector2.right * walkSpeed *Time.fixedDeltaTime;
+        if(!_isFacingRight){
+            nextPoint *= -1;
+        }
         if(!_isWait){
-            _rb.MovePosition((Vector2)transform.position +Vector2.right * walkSpeed *Time.fixedDeltaTime);
+            _rb.MovePosition((Vector2)transform.position + nextPoint);
         }
         
+    }
+    private void Wait(){
+        _waitTime -= Time.deltaTime;
+            if(_waitTime <0f){
+                _waitTime = timeToWait;
+                _isWait = false;
+                Flip();
+            }
+    }
+    private bool ShouldWait(){
+        bool isOutOfRightBoundary = _isFacingRight && transform.position.x >= _RightBoundaryPosition.x;
+        bool isOutOfLeftBoundary = !_isFacingRight && transform.position.x <= _LeftBoundaryPosition.x;
+        return isOutOfRightBoundary || isOutOfLeftBoundary;
     }
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(_LeftBoundaryPosition, _RightBoundaryPosition);
+    }
+    void Flip(){
+        _isFacingRight  = !_isFacingRight;
+        Vector3 playerscale = transform.localScale;
+        playerscale.x *= -1;
+        transform.localScale = playerscale;
     }
 }
