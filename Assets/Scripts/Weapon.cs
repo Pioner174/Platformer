385ae3c3dddf5b3aab3f42c2c428c86872215109
Roman <r.matanin@rfsrf.ru>
2021-03-17 15:17:46 +0500

    61 Player have attack it's worked

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private AttackController _attackController;
+
+    private void Start() {
+        _attackController = transform.root.GetComponent<AttackController>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        EnemyController enemyController = other.GetComponent<EnemyController>();
+        if(enemyController != null && _attackController.IsAttack){
+            Debug.Log("Hit!");
+        }
+    }
+}
