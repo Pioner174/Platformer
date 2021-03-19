d0f3bda5e6382a2381c12ce87bbb759493a8c90b
Roman <r.matanin@rfsrf.ru>
2021-03-19 16:14:12 +0500

    Add Player health

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private float damage = 5f;
+    private void OnCollisionEnter2D(Collision2D other) {
+       PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+       if(playerHealth != null){
+           playerHealth.ReduceHealth(damage);
+       }
+   }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+   [SerializeField] private float health = 100f;
+    [SerializeField] private Animator _animator;
+
+   
+
+    public void ReduceHealth(float damage){
+        _animator.SetTrigger("takeDamage");
+        health -= damage;
+
+        if (health<=0f)
+        Die();
+    }
+
+    private void Die(){
+        gameObject.SetActive(false);
+    }
+}
