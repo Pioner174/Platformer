13a474fcb595ac200202fb7e9809914fe836f99b
Roman <r.matanin@rfsrf.ru>
2021-03-25 17:18:43 +0500

    70 Linking health to a slider

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
-   [SerializeField] private float health = 100f;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private float totalHealth = 100f;
     [SerializeField] private Animator _animator;
-
-   
-
+    private float _health;
+    
+    private void Start() {
+        _health = totalHealth;
+    }
+    private void Update() {
+        healthSlider.value = _health/totalHealth;
+    }
     public void ReduceHealth(float damage){
         _animator.SetTrigger("takeDamage");
-        health -= damage;
+        _health -= damage;
 
-        if (health<=0f)
+        if (_health<=0f)
         Die();
     }
 
