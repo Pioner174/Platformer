9e186d5ec7ad35ea49518bbc64117d323d3f2483
Roman <r.matanin@rfsrf.ru>
2021-04-14 09:01:59 +0500

    94 add new next level canvas

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public void NextLevelHandler (){
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log(scene.buildIndex);
+        SceneManager.LoadScene(1);
+    }
+}
